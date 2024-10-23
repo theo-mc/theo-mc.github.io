e1236b998ba48a4100fd3691057156ea48934362
theo-mc <tmcaleer2k18@gmail.com>
2024-10-23 02:03:01 +0100

    linked interactive CV to home page and added a static CV aswell

---
+++ navigation.js
@@ -0,0 +1,12 @@
+// Get each button by ID
+document.getElementById("button-1").onclick = function () {
+  window.location.href = "terminal/index.html";
+};
+
+document.getElementById("button-2").onclick = function () {
+  window.location.href = "resume/index.html";
+};
+
+document.getElementById("button-3").onclick = function () {
+  window.location.href = "projects.html";
+};
