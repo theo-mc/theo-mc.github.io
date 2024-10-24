d5b4b8cc8d47461e0a11ce58a09e6a7af9736ac0
theo-mc <tmcaleer2k18@gmail.com>
2024-10-24 01:22:40 +0100

    added projects page

--- terminal/resume.js
+++ terminal/resume.js
@@ -1,108 +1,103 @@
 export const resume = {
-    header: {
-        name: "Theo McAleer",
-        contact: {
-            phone: "+44 07715 681068",
-            email: "tmcaleer2k18@gmail.com",
-            github: "github.com/theo-mc",
-        },
-        personalStatement: 
-            "Passionate computer science student with strong problem-solving and programming skills. Seeking a challenging role in software development.",
+  header: {
+    name: "Theo McAleer",
+    contact: {
+      phone: "+44 07715 681068",
+      email: "tmcaleer2k18@gmail.com",
+      github: "github.com/theo-mc",
     },
-    experience: [
-        // {
-        //     title: "Senior Software Developer",
-        //     company: "Tech Innovators Inc.",
-        //     period: "Jan 2020 - Present",
-        //     description:
-        //         "Lead development of cutting-edge web applications using React, Node.js, and GraphQL.",
-        //     achievements: [
-        //         "Architected and implemented a microservices-based backend that improved system scalability by 200%",
-        //         "Mentored junior developers, increasing team productivity by 30%",
-        //         "Introduced automated testing practices, reducing bug reports by 50%",
-        //     ],
-        // },
-        // {
-        //     title: "Full Stack Developer",
-        //     company: "Digital Solutions Co.",
-        //     period: "Jun 2018 - Dec 2019",
-        //     description:
-        //         "Developed and maintained full-stack applications using MERN stack and implemented CI/CD pipelines.",
-        //     achievements: [
-        //         "Optimized database queries, resulting in a 40% reduction in API response times",
-        //         "Implemented responsive design principles, improving mobile user engagement by 25%",
-        //         "Contributed to open-source projects, gaining recognition in the developer community",
-        //     ],
-        // },
-        // {
-        //     title: "Junior Web Developer",
-        //     company: "StartUp Ventures",
-        //     period: "Sep 2016 - May 2018",
-        //     description:
-        //         "Assisted in the development of web applications and gained experience in agile methodologies.",
-        //     achievements: [
-        //         "Developed a custom CMS that streamlined content management processes by 60%",
-        //         "Collaborated with UX designers to implement intuitive user interfaces",
-        //         "Participated in code reviews and adopted best practices for clean, maintainable code",
-        //     ],
-        // },
-    ],
-    skills: {
-        technical: {
-            "Python": 6,
-            "C#": 5,
-            "Java": 6,
-            "JavaScript": 7,
-            "HTML": 8,
-            "CSS": 7,
-            "SQL": 7,
-            "Linux": 8,
-            "Git": 6,
-        },
-        soft: [
-            "Problem-solving",
-            "Teamwork",
-            "Communication"
-        ]
+    personalStatement:
+      "Passionate computer science student with strong problem-solving and programming skills. Seeking a challenging role in software development.",
+  },
+  experience: [
+    // {
+    //     title: "Senior Software Developer",
+    //     company: "Tech Innovators Inc.",
+    //     period: "Jan 2020 - Present",
+    //     description:
+    //         "Lead development of cutting-edge web applications using React, Node.js, and GraphQL.",
+    //     achievements: [
+    //         "Architected and implemented a microservices-based backend that improved system scalability by 200%",
+    //         "Mentored junior developers, increasing team productivity by 30%",
+    //         "Introduced automated testing practices, reducing bug reports by 50%",
+    //     ],
+    // },
+    // {
+    //     title: "Full Stack Developer",
+    //     company: "Digital Solutions Co.",
+    //     period: "Jun 2018 - Dec 2019",
+    //     description:
+    //         "Developed and maintained full-stack applications using MERN stack and implemented CI/CD pipelines.",
+    //     achievements: [
+    //         "Optimized database queries, resulting in a 40% reduction in API response times",
+    //         "Implemented responsive design principles, improving mobile user engagement by 25%",
+    //         "Contributed to open-source projects, gaining recognition in the developer community",
+    //     ],
+    // },
+    // {
+    //     title: "Junior Web Developer",
+    //     company: "StartUp Ventures",
+    //     period: "Sep 2016 - May 2018",
+    //     description:
+    //         "Assisted in the development of web applications and gained experience in agile methodologies.",
+    //     achievements: [
+    //         "Developed a custom CMS that streamlined content management processes by 60%",
+    //         "Collaborated with UX designers to implement intuitive user interfaces",
+    //         "Participated in code reviews and adopted best practices for clean, maintainable code",
+    //     ],
+    // },
+  ],
+  skills: {
+    technical: {
+      Python: 6,
+      "C#": 5,
+      "C++": 6,
+      Java: 6,
+      JavaScript: 7,
+      HTML: 8,
+      CSS: 7,
+      SQL: 7,
+      Linux: 8,
+      Git: 6,
     },
-    projects: [
-        {
-            title: "Personal Resume Website",
-            description: "Built a personal website using base HTML, CSS and JS and deployed it to Github Pages.",
-            technologies: [
-                "HTML",
-                "CSS",
-                "JavaScript",
-            ],
-            highlights: [
-                "Interactive terminal",
-                "Theme switcher for entire webpage"
-            ],
-        }
-    ],
-    education: [
-        {
-            school: "Queens University Of Belfast",
-            qualification: "Bachelor of Computer Science",
-            graduationYear: "2027",
-            modules: [
-                "Databases",
-                "Programming",
-                "Cyber Security",
-                "Web Development",
-                "Computer Architecture",
-                "Math for Computer Science",
-            ]
-        },
-        {
-            school: "Christian Brothers Grammer School",
-            qualification: "A Levels",
-            graduationYear: "2023",
-            courses: [
-                { name: "Technology & Design",         grade: "A"},
-                { name: "Software System Development", grade: "B"},
-                { name: "Business",                    grade: "C"}
-            ]
-        }
-    ]
-}
+    soft: ["Problem-solving", "Teamwork", "Communication"],
+  },
+  projects: [
+    {
+      title: "Personal Resume Website",
+      description:
+        "Built a multipage portfolio website using base HTML, CSS and JS and deployed it to Github Pages.",
+      technologies: ["HTML", "CSS", "JavaScript"],
+      highlights: [
+        "Interactive terminal",
+        "Theme switcher for entire webpage",
+        "dynamic scaling grid animation",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "Queens University Of Belfast",
+      qualification: "Bachelor of Computer Science",
+      graduationYear: "2027",
+      modules: [
+        "Databases",
+        "Programming",
+        "Cyber Security",
+        "Web Development",
+        "Computer Architecture",
+        "Math for Computer Science",
+      ],
+    },
+    {
+      school: "Christian Brothers Grammer School",
+      qualification: "A Levels",
+      graduationYear: "2023",
+      courses: [
+        { name: "Technology & Design", grade: "A" },
+        { name: "Software System Development", grade: "B" },
+        { name: "Business", grade: "C" },
+      ],
+    },
+  ],
+};
